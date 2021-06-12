87f2201e903f32464d5610614b57c10b472713ae
rurushu <olymangue@gmail.com>
2021-06-12 03:27:49 +0200

    Add: Covert boolean to string kata

---
+++ bool2str/bool2str.js
@@ -0,0 +1,15 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+
+/*
+  Problem
+
+  Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+// Solution
+function boolToWord(bool) {
+  return bool ? 'Yes' : 'No';
+}
+
+boolToWord(true);
