ee9e858b3cbfaa0630deb0dc1cbff407cdce3e03
Restyled.io <commits@restyled.io>
2021-06-12 01:28:54 +0000

    Restyled by clang-format

--- bool2str/bool2str.js
+++ bool2str/bool2str.js
@@ -3,13 +3,12 @@
 /*
   Problem
 
-  Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+  Complete the method that takes a boolean value and return a "Yes" string for
+  true, or a "No" string for false.
 
 */
 
 // Solution
-function boolToWord(bool) {
-  return bool ? 'Yes' : 'No';
-}
+function boolToWord(bool) { return bool ? 'Yes' : 'No'; }
 
 boolToWord(true);
